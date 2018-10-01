86d98ca38d4b204bc8058570c894ddb058eeb924
RedazioneFPA <39738669+RedazioneFPA@users.noreply.github.com>
2018-10-01 21:59:36 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -40,8 +40,8 @@ def setup(app):
 """
 #latex_documents = []
 
-latex_logo = "static/logo.jpg"
-html_logo = "static/logo.jpg"
+latex_logo = "static/fpa_digital360_negativo.png"
+html_logo = "static/fpa_digital360_negativo.png"
 
 
 # Adding Custom CSS or JavaScript to a Sphinx Project: al seguente link ci sono esempi
